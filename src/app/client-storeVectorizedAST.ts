4c91e4b4cc7b20721db55fe33a3e8ca93037f853
baristaGeek <estebanvargas94@gmail.com>
2023-10-16 21:23:12 -0500

    move server side

---
+++ src/app/client-storeVectorizedAST.ts
@@ -0,0 +1,30 @@
+import { pipeline } from "@xenova/transformers";
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!, 
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
+
+export default async function storeVectorizedAST(body: string, ast: string): Promise<void> {
+  const generateEmbedding = await pipeline(
+    "feature-extraction",
+    "Supabase/gte-small"
+  );
+
+  // // Generate a vector using Transformers.js
+  const output = await generateEmbedding(body, {
+    pooling: "mean",
+    normalize: true,
+  });
+
+  // // Extract the embedding output
+  const embedding = Array.from(output.data);
+
+  // Store the vector in Postgres
+  const { data, error } = await supabase.from("documents").insert({
+    body,
+    embedding,
+  });
+};
