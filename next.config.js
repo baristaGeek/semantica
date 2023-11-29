1e676f6c54e2bd038f10c3e8eda911a51e503f56
baristaGeek <estebanvargas94@gmail.com>
2023-11-28 19:39:04 -0500

    Add xenova transformers nextconfig

--- next.config.js
+++ next.config.js
@@ -9,8 +9,16 @@ module.exports = {
     config.module.rules.push({
       test: /\.node$/,
       use: 'node-loader', 
+
+      output: 'standalone', // Feel free to modify/remove this option
+    
+      // Indicate that these packages should not be bundled by webpack
+      experimental: {
+          serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+      },
     });
 
+    
     return config;
   },
 };
